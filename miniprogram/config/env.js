c11d9e83ebeb48bc405cced084c68a71b55a970d
lumos224 <792938429@qq.com>
2026-05-27 18:31:17 +0800

    配置小程序演示后端地址

--- miniprogram/config/env.js
+++ miniprogram/config/env.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Fill this with the deployed HTTPS backend, for example:
   // apiBaseUrl: 'https://student-service.example.edu.cn/api'
-  apiBaseUrl: '',
+  apiBaseUrl: 'http://39.106.161.170:8080',
   tokenStorageKey: 'student_service_token'
 }
